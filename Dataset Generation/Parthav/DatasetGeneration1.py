02c2797972de027c9a805cef015652b7cbb92d39
Parthav Patel <parthavpatel@Parthavs-MacBook-Air.local>
2020-10-24 01:49:13 -0700

    Added changes to noise generation code

--- Dataset Generation/Parthav/DatasetGeneration1.py
+++ Dataset Generation/Parthav/DatasetGeneration1.py
@@ -9,6 +9,7 @@ Created on Sun Oct 18 16:26:55 2020
 import json 
 import random
 from random import randint
+import re
   
 # Opening JSON file 
 f = open('sample1.json',) 
@@ -26,7 +27,38 @@ noise_content_list = content.split("\n")
 my_file.close()
 
 
+def return_ans_value(options,correct_option):
+    list = options.split(',')
+    for l in list:
+        if correct_option in l:
+            return re.sub('[A-Za-z%$]','',l.split(')')[1].strip().replace(" ", ""))
 
+def change_question_format(ans):
+    temp_ans = []
+    i = 0
+    count = 0
+    for item in ans:
+        try:
+            if re.match("[+,-]*\d+?\.*\d*", item['correct']):
+            #if item['correct'].replace('.','',1).isdigit():
+                temp_dict = {}
+                temp_dict["ID"]= i 
+                temp_dict["Passage"]= ""
+                temp_dict["Question"] = item['Problem'] 
+                temp_dict["Equation"] =  item["annotated_formula"]
+                temp_dict["Answer"] =  item['correct']
+                temp_dict["Reasoning Type"] = "" 
+                temp_dict["Type"] = "PP_Random_noise_insertion"
+                temp_ans.append(temp_dict)
+                i += 1
+        except:
+            print(item['correct'])
+
+    print(count)        
+    return temp_ans
+        
+    
+    
 ans = []
 for d1 in data:
     dictionary = {}
@@ -35,11 +67,14 @@ for d1 in data:
     temp_question_list = dictionary['Problem'].split('.')
     temp_question_list.insert(randint(1,len(temp_question_list)),random_sentence)
     dictionary['Problem'] = '.'.join(sentence for sentence in temp_question_list)
+    dictionary['correct'] = return_ans_value(dictionary['options'],dictionary['correct'])
     ans.append(dictionary)
     
+final_list = change_question_format(ans)
+
 
 with open('Sample_data_generated.json', 'w') as fp:
-    json.dump(ans, fp)
+    json.dump(final_list, fp)
     
 
 
--- Dataset Generation/Parthav/Noise_dataset_creation.py
+++ Dataset Generation/Parthav/Noise_dataset_creation.py
@@ -7,41 +7,55 @@ Created on Tue Oct 20 00:19:07 2020
 """
 
 import json 
-import random
-from random import randint
-  
-# Opening JSON file 
-f = open('sample1.json',) 
-  
-# returns JSON object as  
-# a dictionary 
-data = json.load(f) 
+import os
+import re
 
+def noise_sentence_generation(file_name):  
+    data = []
+    noise_content_list = []
+    for line in open(file_name, 'r'):
+        data.append(json.loads(line))
     
-# Closing file 
-f.close() 
- 
-# Opening JSON file 
-f = open('sample2.json',) 
-  
-# returns JSON object as  
-# a dictionary 
-data1 = json.load(f) 
+    cleaned_data = data_cleaning(data)
+    if os.path.isfile('noise_dataset.txt'):
+        my_file = open("noise_dataset.txt", "r")
+        content = my_file.read()
+        noise_content_list = content.split("\n")
+        my_file.close()
+    
+    noise_content_list += cleaned_data
 
-  
-# Closing file 
-f.close()
+    with open('noise_dataset.txt', 'w') as f:
+        for item in noise_content_list:
+            f.write("%s\n" % item)
 
-data_combined = data + data1 
+def data_cleaning(data):
+    l = []
+    for d in data:
+        if 'question' in d.keys():
+            key_label = 'question'
+        else:
+            key_label = 'problem'
+        temp_l = d[key_label].split('.')
+        for t in temp_l:
+            t = t.strip()
+            if len(t) > 10:
+                if re.match(r"[what|What|Find|find]",t):
+                    continue
+                elif not re.search('[a-zA-Z]', t):
+                    continue
+                if '?' in t or t.strip().isdigit() or t.isspace() or len(t) < 10:
+                    continue
+                else:
+                    l.append(t)
+            else:
+                print("true")
+    return l
 
-l = []
-for d in data_combined:
-    temp_l = d['Problem'].split('.')
-    for t in temp_l:
-        if '?' in t or t.strip().isdigit() or t.isspace() or len(t)==0 or 'what' in t:
-            temp_l.remove(t)
-    l = l + temp_l
 
-with open('noise_dataset.txt', 'w') as f:
-    for item in l:
-        f.write("%s\n" % item)
+if __name__ == '__main__':
+    #Change the file name to insert new noise sentences in the noise sentence dataset
+    file_name = 'sample4.json'
+    noise_sentence_generation(file_name)
+    
+
